47b99affcacc9a883e442d0206400cb5a1247d5c
JanEggers <jan.eggers@ferag.com>
2017-08-27 10:32:24 +0200

    updated client api

--- Playground.Client/Generate.cmd
+++ Playground.Client/Generate.cmd
@@ -1,1 +1,3 @@
-autorest -Input swagger.json -CodeGenerator CSharp -OutputDirectory Generated -Namespace Playground.Client.Generated
+del Generated /F /S /Q 
+
+node_modules\\.bin\\autorest.cmd  -Input swagger.json -CodeGenerator CSharp -OutputDirectory Generated -Namespace Playground.Client.Generated
