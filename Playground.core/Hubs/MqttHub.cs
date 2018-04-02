295465635bbb62373af8ecc2f2ed13cbf97b898d
JanEggers <jan.Eggers@ferag.com>
2018-04-02 16:11:06 +0200

    implemented subscription filtering

--- Playground.core/Hubs/MqttHub.cs
+++ Playground.core/Hubs/MqttHub.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using MQTTnet.Packets;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
 using System;
+using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +12,13 @@ namespace Playground.core.Hubs
 {
     public class MqttHub : Hub
     {
+        private readonly ILogger<MqttHub> logger;
+
+        public MqttHub(ILogger<MqttHub> logger)
+        {
+            this.logger = logger;
+        }
+
         public MqttConnAckPacket Connect(MqttConnectPacket connect)
         {
             return new MqttConnAckPacket()
@@ -21,9 +32,32 @@ namespace Playground.core.Hubs
         {
         }
 
-        public IObservable<object> OnSubscribe(MqttSubscribePacket sub)
+        public IObservable<MqttBasePacket> OnSubscribe(IObservable<MqttPublishPacket> packets, MqttSubscribePacket sub)
         {
-            return Observable.Return<object>(null);
+            var ack = new MqttSubAckPacket()
+            {
+                PacketIdentifier = sub.PacketIdentifier,
+            };
+
+            foreach (var topic in sub.TopicFilters)
+            {
+                ack.SubscribeReturnCodes.Add(MqttSubscribeReturnCode.SuccessMaximumQoS0);
+            }
+
+
+            var filteredPublishPackets = packets
+                //.Do(p => logger.LogInformation($"publish {p.Topic}"))
+                .Where(p =>
+                {
+                    var result = sub.TopicFilters.Any(f => MqttTopicFilterComparer.IsMatch(p.Topic, f.Topic));
+
+                    // logger.LogInformation($"filter {p.Topic}: {result}");
+
+                    return result;
+                });
+
+            return Observable.Return<MqttBasePacket>(ack)
+                .Concat(filteredPublishPackets);
         }
 
         public override Task OnConnectedAsync()
--- Playground.core/Hubs/MqttHubProtocol.cs
+++ Playground.core/Hubs/MqttHubProtocol.cs
@@ -2,9 +2,12 @@
 using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reactive.Subjects;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.SignalR.Internal;
 using Microsoft.AspNetCore.SignalR.Internal.Protocol;
+using Microsoft.Extensions.Logging;
 using MQTTnet.Packets;
 using MQTTnet.Serializer;
 
@@ -13,7 +16,7 @@ namespace Playground.core.Hubs
     public class MqttHubProtocol : IHubProtocol
     {
         private readonly MqttPacketSerializer _serializer;
-        private readonly ConnectionContext connectionContext;
+        private readonly ILogger<MqttHubProtocol> _logger;
 
         public string Name => "MQTT";
 
@@ -21,9 +24,10 @@ namespace Playground.core.Hubs
 
         public TransferFormat TransferFormat => TransferFormat.Binary;
 
-        public MqttHubProtocol(MqttPacketSerializer serializer)
+        public MqttHubProtocol(MqttPacketSerializer serializer, ILogger<MqttHubProtocol> logger)
         {
             _serializer = serializer;
+            _logger = logger;
         }
 
         public bool IsVersionSupported(int version)
@@ -31,8 +35,8 @@ namespace Playground.core.Hubs
             return true;
         }
 
-        private Dictionary<string, MqttSubscribePacket> _subscriptions = new Dictionary<string, MqttSubscribePacket>();
-
+        private Subject<MqttPublishPacket> _packets = new Subject<MqttPublishPacket>();
+        
         public bool TryParseMessage(ref ReadOnlySequence<byte> input, IInvocationBinder binder, out HubMessage message)
         {
             MqttBasePacket packet;
@@ -46,14 +50,17 @@ namespace Playground.core.Hubs
                 switch (packet)
                 {
                     case MqttConnectPacket connect:
+                        _logger.LogInformation($"connect {invokation}");
                         message = new InvocationMessage(invokation, nameof(MqttHub.Connect), null, connect);
                         break;
                     case MqttPublishPacket publish:
+                        _logger.LogInformation($"publish {invokation} {publish.Topic}");
+                        _packets.OnNext(publish);
                         message = new InvocationMessage(invokation, nameof(MqttHub.OnPublish), null, publish);
                         break;
                     case MqttSubscribePacket subscribe:
-                        message = new StreamInvocationMessage(invokation, nameof(MqttHub.OnSubscribe), null, subscribe);
-                        _subscriptions[invokation] = subscribe;
+                        _logger.LogInformation($"subscribe {invokation} {string.Join(", ", subscribe.TopicFilters.Select(t => t.Topic))}");
+                        message = new StreamInvocationMessage(invokation, nameof(MqttHub.OnSubscribe), null, _packets, subscribe);
                         break;
                     case null:
                         break;
@@ -69,18 +76,28 @@ namespace Playground.core.Hubs
             switch (message)
             {
                 case CompletionMessage completion:
+                    _logger.LogInformation($"complete {completion.InvocationId}");
                     WriteCompletionMessage(completion, output);
                     break;
+                case StreamItemMessage streamItem:
+                    _logger.LogInformation($"streamItem {streamItem.InvocationId}");
+                    WriteStreamMessage(streamItem, output);
+                    break;
                 case CloseMessage close:
+                    _logger.LogInformation($"close");
                     WriteMqttPacket(new MqttDisconnectPacket() { }, output);
                     break;
-                case StreamItemMessage streamItem:
-                    if (_subscriptions.TryGetValue(streamItem.InvocationId, out var subscription)) 
-                    {
-                        WriteMqttPacket(new MqttPublishPacket() {
-                            Topic = subscription.TopicFilters[0].Topic
-                        }, output);
-                    }
+                default:
+                    break;
+            }
+        }
+
+        public void WriteStreamMessage(StreamItemMessage streamItem, Stream output)
+        {
+            switch (streamItem.Item)
+            {
+                case MqttBasePacket mqtt:
+                    WriteMqttPacket(mqtt, output);
                     break;
                 default:
                     break;
