09f627f15f32763b1e9bd7a7c68745e99a7ac647
JanEggers <jan.eggers@ferag.com>
2018-07-01 15:37:36 +0200

    fixed swagger not opening

--- Playground.core/Startup.cs
+++ Playground.core/Startup.cs
@@ -23,7 +23,9 @@ using Serilog;
 using Serilog.Events;
 
 using Swashbuckle.AspNetCore.Swagger;
-
+using Microsoft.AspNet.OData.Formatter;
+using Microsoft.Net.Http.Headers;
+using System.Linq;
 
 namespace Playground.core
 {
@@ -120,6 +122,21 @@ namespace Playground.core
             services.AddTransient<PlaygroundModelBuilder>();
 
             services.AddOData();
+
+            services.AddMvcCore(options => {
+
+
+                // Workaround: https://github.com/OData/WebApi/issues/1177
+                foreach (var outputFormatter in options.OutputFormatters.OfType<ODataOutputFormatter>().Where(_ => _.SupportedMediaTypes.Count == 0))
+                {
+                    outputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+                }
+                foreach (var inputFormatter in options.InputFormatters.OfType<ODataInputFormatter>().Where(_ => _.SupportedMediaTypes.Count == 0))
+                {
+                    inputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+                }
+                //endofworkaround
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -170,6 +187,9 @@ namespace Playground.core
                 builder.MapODataServiceRoute("odata", "odata", edmModel);
 
                 builder.MapRoute("default", "api/{controller}/{action}");
+                
+                // Workaround: https://github.com/OData/WebApi/issues/1175
+                builder.EnableDependencyInjection();
             } );
             
             app.UseSwagger();
