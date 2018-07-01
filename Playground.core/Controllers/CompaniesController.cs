HYDPublic/Playground-1

odata working but meh

Playground.core/Controllers/CompaniesController.cs
@@ -9,12 +9,14 @@ using Playground.core.Models;
 using AutoMapper.QueryableExtensions;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.OData;
 using AutoMapper;
 
 namespace Playground.core.Controllers
 {
-    [Authorize]
-    [Route("api/[controller]")]
+    //[Authorize]
+    [EnableQuery]
+    [Route("odata/Companies")]
     public class CompaniesController : EntityController<Company,int>
     {
         private IConfigurationProvider m_mapperConfig;

Playground.core/Controllers/SitesController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.JsonPatch;
 
 using Playground.core.Models;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.OData;
 
 namespace Playground.core.Controllers
 {
-    [Authorize]
-    [Route("api/[controller]")]
+    //[Authorize]
+    [EnableQuery]
+    [Route("odata/Sites")]
     public class SitesController : EntityController<Site, int>  
     {
         public SitesController(PlaygroundContext ctx)

Playground.core/Startup.cs
@@ -17,7 +17,8 @@ using AspNet.Security.OpenIdConnect.Primitives;
 using Playground.core.Models;
 using Playground.core.Hubs;
 using Playground.core.Services;
-
+using Microsoft.AspNetCore.OData.Extensions;
+using Playground.core.Odata;
 using Serilog;
 using Serilog.Events;
 
@@ -116,6 +117,9 @@ namespace Playground.core
             services.AddSignalR();
 
             services.AddTransient<SeedService>();
+            services.AddTransient<PlaygroundModelBuilder>();
+
+            services.AddOData();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -151,8 +155,11 @@ namespace Playground.core
             });
 
             app.UseStaticFiles();
-
-            app.UseMvcWithDefaultRoute();
+            
+            app.UseMvc( builder => {
+                builder.MapODataRoute( app.ApplicationServices.GetRequiredService<PlaygroundModelBuilder>().GetEdmModel() );
+                builder.MapRoute("default", "api/{controller}/{action}");
+            } );
 
             app.UseSwagger();
 

JanEggers/Playground

Playground.core/Controllers/CompaniesController.cs
@@ -9,12 +9,14 @@ using Playground.core.Models;
 using AutoMapper.QueryableExtensions;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.AspNet.OData;
 using AutoMapper;
 
 namespace Playground.core.Controllers
 {
-    [Authorize]
-    [Route("api/[controller]")]
+    //[Authorize]
+    [EnableQuery]
+    [Route("odata/Companies")]
     public class CompaniesController : EntityController<Company,int>
     {
         private IConfigurationProvider m_mapperConfig;

Playground.core/Controllers/SitesController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.JsonPatch;
 
 using Playground.core.Models;
 using System.Collections.Generic;
+using Microsoft.AspNet.OData;
 
 namespace Playground.core.Controllers
 {
-    [Authorize]
-    [Route("api/[controller]")]
+    //[Authorize]
+    [EnableQuery]
+    [Route("odata/Sites")]
     public class SitesController : EntityController<Site, int>  
     {
         public SitesController(PlaygroundContext ctx)

Playground.core/Startup.cs
@@ -17,7 +17,8 @@ using AspNet.Security.OpenIdConnect.Primitives;
 using Playground.core.Models;
 using Playground.core.Hubs;
 using Playground.core.Services;
-
+using Microsoft.AspNet.OData.Extensions;
+using Playground.core.Odata;
 using Serilog;
 using Serilog.Events;
 
@@ -116,6 +117,9 @@ namespace Playground.core
             services.AddSignalR();
 
             services.AddTransient<SeedService>();
+            services.AddTransient<PlaygroundModelBuilder>();
+
+            services.AddOData();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -151,8 +155,11 @@ namespace Playground.core
             });
 
             app.UseStaticFiles();
-
-            app.UseMvcWithDefaultRoute();
+            
+            app.UseMvc( builder => {
+                builder.MapODataServiceRoute("odata", "odata", app.ApplicationServices.GetRequiredService<PlaygroundModelBuilder>().GetEdmModel() );
+                builder.MapRoute("default", "api/{controller}/{action}");
+            } );
 
             app.UseSwagger();
 