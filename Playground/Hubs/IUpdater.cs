f3b346058a2ae84670b56ef036b2387fb63e723e
JanEggers <jan.Eggers@ferag.com>
2020-07-31 16:41:38 +0200

    removed aspnet classic

--- Playground/Hubs/IUpdater.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Playground.Hubs
-{
-    public interface IUpdater
-    {
-        void Update(string selfLink, Dictionary<string, object> changes);
-    }
-}
